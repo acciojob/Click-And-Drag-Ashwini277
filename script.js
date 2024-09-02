1fd32a76c7f36da6f1b426f05e8eece675962bd6
Ashwini277 <devarushiashwini@gmail.com>
2024-09-02 10:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 it('drag & drop works correctly', () => {
-    cy.get('.items')
-        .should('exist') // Ensure the element exists before proceeding
+    cy.get('.items', { timeout: 10000 }) // Increase timeout to wait for the element
+        .should('exist') // Ensure the element exists
+        .should('be.visible') // Ensure the element is visible
         .trigger('mousedown', { which: 1, pageX: 493, pageY: 391 })
         .trigger('mousemove', { pageX: 271, pageY: 391 })
         .trigger('mouseup', { force: true });
 
-    cy.get('.items')
-        .should($items => {
-            expect($items[0].scrollLeft).to.be.greaterThan(0);
-        });
+    cy.get('.items').should($items => {
+        expect($items[0].scrollLeft).to.be.greaterThan(0);
+    });
 });
